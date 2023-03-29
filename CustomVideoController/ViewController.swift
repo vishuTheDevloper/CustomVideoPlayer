fed96aa83625409efc97faefbee0190135051038
kapil <kapild@solidappmaker.com>
2023-03-29 11:51:00 +0530

    Initial Commit

---
+++ CustomVideoController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomVideoController
+//
+//  Created by John on 29/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
